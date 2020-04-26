3dcf8b16ed6e566958af489c93de961775d511e0
vagnersabadi <vagnerbuzatta@hotmail.com>
2020-04-26 10:51:19 -0300

    feat(): schendule

--- src/app/pages/schedule/schedule.page.ts
+++ src/app/pages/schedule/schedule.page.ts
@@ -10,10 +10,10 @@ import { NewTaskPage } from '../task/new-task/new-task.page';
 export class SchedulePage implements OnInit {
 
   tasks: any[] = [
-    { hour: 1, colorHex: '#F9BE7C', title: 'Project Research' },
-    { hour: 5, colorHex: '#e77480', title: 'Call' },
-    { hour: 8, colorHex: '#7494e7', title: 'Make tasks for the medical app' },
-    { hour: 1, colorHex: '#459ea1', title: 'Design meeting' },
+    { hour: 1, colorHex: '#F9BE7C', title: 'Project Research', description: undefined },
+    { hour: 5, colorHex: '#e77480', title: 'Call', description: 'Discuss with the customer of the medical apllication the references that he send.' },
+    { hour: 8, colorHex: '#7494e7', title: 'Make tasks for the medical app', description: undefined },
+    { hour: 1, colorHex: '#459ea1', title: 'Design meeting', description: 'Discuss with designer new tasks for the medical appp. Tell how the call went.' },
 
   ];
 
